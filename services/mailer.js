ec9274f2f951ccdc0be334e02312bec9b29c4abb
Abraham <cedenoabraham@gmail.com>
2019-10-13 19:11:08 -0300

    mail service

---
+++ services/mailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+
+class Mailer {
+    options;
+    transporter;
+
+    constructor(from, to, subject, text){
+        this.options = {
+            from,
+            to,
+            subject,
+            text
+        }
+
+        this.transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+              user: process.env.MAIL_ACCOUNT,
+              pass: process.env.MAIL_PWD
+            }
+        });
+    }
+
+    send(){
+        return new Promise((reject, resolve) => {
+            this.transporter.sendMail(this.options, function(error, info) {
+                if (error) {
+                  reject(error);
+                } else {
+                  resolve("Email successfully sent!");
+                }
+            });
+        })
+    }
+}
+
+export default Mailer;
