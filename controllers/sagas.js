f7e1bd9200c819fd6feb31fbfbc340500b6ad3d1
Abraham <cedenoabraham@gmail.com>
2020-10-25 23:23:49 -0300

    sagas controller

--- controllers/sagas.js
+++ controllers/sagas.js
@@ -9,27 +9,72 @@ const Sagas = mongoose.model("sagas");
 module.exports = {
     getAll: () => {
         return new Promise((resolve, reject) => {
-
+            client.get("sagas", (err, result) => {
+                if (err) reject(err);
+                if (result) {
+                    resolve(JSON.parse(result));
+                } else {
+                    Sagas.find({}, (err, films) => {
+                        if (err) throw err;
+                        client.setex(
+                            "sagas",
+                            process.env.REDIS_EXP_TIME,
+                            JSON.stringify(films)
+                        );
+                        resolve(films);
+                    });
+                }
+            });
         })
     },
     get: (name) => {
         return new Promise((resolve, reject) => {
-
+            client.get(name, (err, result) => {
+                if (err) reject(err);
+                if (result) {
+                    resolve(JSON.parse(result));
+                } else {
+                    Sagas.findOne(
+                        { title: { $regex: new RegExp(`^${name}`, "i") } },
+                        (err, film) => {
+                            if (err) reject(err);
+                            client.setex(
+                                name,
+                                process.env.REDIS_EXP_TIME,
+                                JSON.stringify(film)
+                            );
+                            resolve(film);
+                        }
+                    );
+                }
+            });
         })
     },
-    create: (name, description, avatar) => {
+    create: (name, description, image) => {
         return new Promise((resolve, reject) => {
-
-        })
-    },
-    update: (id, body) => {
-        return new Promise((resolve, reject) => {
-
+            Sagas.create(
+                {
+                    name,
+                    description,
+                    image,
+                },
+                (err, saga) => {
+                    if (err) reject(err);
+                    resolve({
+                        message: "Resource created"
+                    });
+                }
+            );
         })
     },
     delete: (id) => {
         return new Promise((resolve, reject) => {
-
+            Sagas.deleteOne({ _id: id }, (err, saga) => {
+                if (err) reject(err);
+                resolve({
+                    message: "Resource deleted"
+                });
+            });
         })
     }
 }
--- routes/sagas.js
+++ routes/sagas.js
@@ -1,57 +1,34 @@
 const express = require("express");
-const redis = require("redis");
-const mongoose = require("mongoose");
 const { check, body } = require("express-validator");
-const config = require("../config");
-
-const client = redis.createClient({ host: config.redis.host });
 const router = express.Router();
-
-const Sagas = mongoose.model("sagas");
 const auth = require("../middleware/auth");
 
 const validate = require("../middleware/validate");
 
-router.get("/", (req, res) => {
-    client.get("sagas", (err, result) => {
-        if (err) throw err;
-        if (result) {
-            res.status(200).json(JSON.parse(result));
-        } else {
-            Sagas.find({}, (err, sagas) => {
-                if (err) throw err;
-                client.setex(
-                    "sagas",
-                    process.env.REDIS_EXP_TIME,
-                    JSON.stringify(sagas)
-                );
-                res.status(200).json(sagas);
-            });
-        }
-    });
+const sagas = require('../controllers/sagas');
+
+router.get("/", async (req, res) => {
+    try {
+        const response = await sagas.getAll();
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(401).json({
+            message: error
+        });
+    }
 });
 
-router.get("/:name", (req, res) => {
+router.get("/:name", async (req, res) => {
     const { name } = req.params;
-    client.get(name, (err, result) => {
-        if (err) throw err;
-        if (result) {
-            res.status(200).json(JSON.parse(result));
-        } else {
-            Sagas.findOne(
-                { name: { $regex: new RegExp(`^${name}`, "i") } },
-                (err, saga) => {
-                    if (err) throw err;
-                    client.setex(
-                        name,
-                        process.env.REDIS_EXP_TIME,
-                        JSON.stringify(saga)
-                    );
-                    res.status(200).json(saga);
-                }
-            );
-        }
-    });
+
+    try {
+        const response = await sagas.get(name);
+        res.status(200).json(response);
+    } catch (error) {
+        res.status(401).json({
+            message: error
+        });
+    }
 });
 
 router.post(
@@ -66,36 +43,34 @@ router.post(
                 .isURL()
         ])
     ],
-    (req, res) => {
+    async (req, res) => {
         const { name, description, image } = req.body;
 
-        Sagas.create(
-            {
-                name,
-                description,
-                image
-            },
-            (err, saga) => {
-                if (err) throw err;
-                res.status(201).json({
-                    message: "Resource created"
-                });
-            }
-        );
+        try {
+            const response = await sagas.create(name, description, image);
+            res.status(200).json(response);
+        } catch (error) {
+            res.status(401).json({
+                message: error
+            });
+        }
     }
 );
 
 router.delete(
     "/:id",
     [auth, validate([check("id").isAlphanumeric()])],
-    (req, res) => {
+    async (req, res) => {
         const { id } = req.params;
-        Sagas.deleteOne({ _id: id }, (err, saga) => {
-            if (err) throw err;
-            res.status(200).json({
-                message: "Resource deleted"
+        
+        try {
+            const response = await sagas.delete(id);
+            res.status(200).json(response);
+        } catch (err) {
+            res.status(422).json({
+                message: err
             });
-        });
+        }
     }
 );
 
